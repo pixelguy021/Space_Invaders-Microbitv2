1136789eeeddb629158ae1ae837b6596609e7159
GenieOmg <85433845+pixelguy021@users.noreply.github.com>
2026-04-27 11:58:38 +0530

    2 states of display with proper display refresh rate and buttons working (to do compare logic)

--- main.s
+++ main.s
@@ -24,6 +24,7 @@ _start:
     ldr r1, =(1<<5)
     str r1, [r0]
     ldr r11,=#0
+    ldr r12,=#0
     @ --- Configure Button A (P0.14) ---
              
     ldr r0, =0x50000738    @ PIN_CNF[14]
@@ -37,15 +38,15 @@ _start:
 
     ldr r0,=0x20000000
     @ row1-5
-    ldr r7,=0b01110
-    str r7,[r0]
-    ldr r7,=0b10101
-    str r7,[r0, #4]
-    ldr r7,=0b10101
-    str r7,[r0, #8]
-    ldr r7,=0b01110
-    str r7,[r0, #12]
     ldr r7,=0b00000
+    str r7,[r0]
+    ldr r7,=0b00000
+    str r7,[r0, #4]
+    ldr r7,=0b00000
+    str r7,[r0, #8]
+    ldr r7,=0b00000
+    str r7,[r0, #12]
+    ldr r7,=0b00100
     str r7,[r0, #16]
 
     add r0,r0,#20 @adderss for the skull 
@@ -66,17 +67,65 @@ _start:
     @ setting state 1 in registers
     @ --- 2. Load Image Data into r1-r5 ---
     @ A '1' bit means LED ON. We use 5 bits (LSB).
-    ldr r1, =0b01010   @ Row 1 ( . X . X . )
-    ldr r2, =0b11111   @ Row 2 ( X X X X X )
-    ldr r3, =0b11111 @ Row 3 ( X X X X X )
-    ldr r4, =0b01110   @ Row 4 ( . X X X . )
-    ldr r5, =0b00100   @ Row 5 ( . . X . . )
+    ldr r1, =0b11111   @ Row 1 ( . X . X . )
+    ldr r2, =0b10001   @ Row 2 ( X X X X X )
+    ldr r3, =0b11011 @ Row 3 ( X X X X X )
+    ldr r4, =0b11111   @ Row 4 ( . X X X . )
+    ldr r5, =0b10001   @ Row 5 ( . . X . . )
 
-    bl loop
+    b game_loop
 
 
+@ bx lr only works one layer deep
+    
+
+game_loop:
+    
+
+    mov r10,#1
+    render:
+       bl loop
+       subs r10,r10,#1
+        bne render
+    
+    bl store_swap 
+    add r12, r12, #1
+    cmp r12, #100        @ Try 100 for ~1.5 seconds
+    it ge
+    blge move_rock   
+    
+    mov r10,#1
+    render1:
+            @ stores 
+        bl loop               @ Call your 5-row scan
+        subs r10, r10, #1
+        bne render1
+        
+    bl store_swap
+    
+    ldr r0, =0x50000510    @ GPIO P0 IN Register
+    ldr r10, [r0]           @ Read all pins on Port 0
+    tst r10, #(1 << 14)     @ Test bit 14 (Button A)
+    it eq                  @ If result is 0 (Equal to zero), button is PRESSED
+    bleq move_left        @ Call your swap function!
+
+
+    tst r10, #(1 << 23)     @ Test bit 23 (Button B)
+    it eq                  @ If result is 0 (Equal to zero), button is PRESSED
+    bleq move_right       @ Call your swap function!
+
+
+    add r11, r11, #1
+    cmp r11, #20         @ Try 100 for ~1.5 seconds
+    it ge
+    blge shoot_up        @ This MUST reset r11 to 0 inside the function
+
+    
+    
+    b game_loop
 
 loop:
+    push {lr}
     @ --- Row 1 ---
     mov r6, r1          @ Get Row 1 data
     ldr r7 ,=(1<<21)    @ Row 1 Pin (P0.21)
@@ -102,22 +151,7 @@ loop:
     ldr r7, =(1<<19)    @ Row 5 Pin (P0.19)
     bl  display_row
 
-    ldr r0, =0x50000510    @ GPIO P0 IN Register
-    ldr r10, [r0]           @ Read all pins on Port 0
-    tst r10, #(1 << 14)     @ Test bit 14 (Button A)
-    it eq                  @ If result is 0 (Equal to zero), button is PRESSED
-    bleq store_swap        @ Call your swap function!
-
-
-    tst r10, #(1 << 23)     @ Test bit 14 (Button A)
-    it eq                  @ If result is 0 (Equal to zero), button is PRESSED
-    bleq get_random_byte       @ Call your swap function!
-
-    add r11, r11, #1
-    cmp r11, #100          @ Try 100 for ~1.5 seconds
-    it ge
-    blge shoot_up        @ This MUST reset r11 to 0 inside the function
-    b loop
+    pop {pc}
 @ --- Subroutine: Display a Row ---
 @ r6 = row data bits, r7 = row pin mask
 display_row: @r7 is my the row which is active,@ r6 stores what to display 
@@ -125,7 +159,7 @@ display_row: @r7 is my the row which is active,@ r6 stores what to display
     @0x5000080C outclr(low) for P1.x (clm 4,5)
     @0x50000508 outset(high) for P0.x
     @0x50000808 outset(high) for P1.x
-
+    
     @setting all clm to high and main value too high , now trying cmp thing 
     ldr r0 ,=0x50000508
     ldr r8 ,=((1<<28)|(1<<11)|(1<<30)|(1<<31))
@@ -145,39 +179,47 @@ display_row: @r7 is my the row which is active,@ r6 stores what to display
     @ Don't like this wanted to implement it diffreently but IT restriction only allow this way, Now it looks like a variable change
         @ clm 4 
         ldr r0 ,=0x5000080C
-        mov r10 ,#1<<5
-        mov r8, #0
+        mov r8,#1<<5
 
         tst r6,#(1<<1)
-        it ne
-        orrne r8, r8, r10
+        ite ne
+        orrne r8,r8,r8
+        andeq r8, r8,#0b11111111111111111111111111011111
         str r8, [r0]
-
+        
         mov r8, #0
         @right most led so clm 5
           @clm 3
-        ldr r10 ,=((1<<31))
+        orr r8,r8,#(1<<31)
         tst r6, #(1<<2)
-        it ne
-        orrne r8, r8, r10
+        ite ne
+        orrne r8,r8,r8
+        andeq r8, r8,#0b01111111111111111111111111111111
+        
+        @ldr r10 ,=(1<<30)
 
-        ldr r10 ,=(1<<30)
+        orr r8,r8,#(1<<30)
         tst r6, #1
-        it ne
-        orrne r8, r8, r10
+        ite ne
+        orrne r8,r8,r8
+        andeq r8, r8,#0b10111111111111111111111111111111
 
       
         @clm 2
-        ldr r10 ,=((1<<11))
+        @ldr r10 ,=((1<<11))
+        orr r8,r8,#(1<<11)
         tst r6, #(1<<3)
-        it ne
-        orrne r8, r8, r10
+        ite ne
+        orrne r8,r8,r8
+        andeq r8, r8,#0b11111111111111111111011111111111
 
         @clm 1
-        ldr r10 ,=((1<<28))
+        @ldr r10 ,=((1<<28))
+        orr r8,r8,#(1<<28)
         tst r6, #(1<<4)
-        it ne
-        orrne r8, r8, r10
+        ite ne
+        orrne r8,r8,r8
+        andeq r8, r8,#0b11101111111111111111111111111111
 
     ldr r0,=0x5000050C
     str r8,[r0]
@@ -262,6 +304,7 @@ wait_rng:
 
  
 move_rock:
+    @ i don't need 7&6 so they will be used
     mov r5, r4    @ Row 4 moves to Row 5
     mov r4, r3    @ Row 3 moves to Row 4
     mov r3, r2    @ Row 2 moves to Row 3
@@ -270,7 +313,7 @@ move_rock:
     push {lr}     @ Save Link Register because we are calling another function
     bl get_random_byte
     mov r1, r0    @ New random row data into Row 1
-    ldr r11,=#0
+    ldr r12,=#0
     pop {pc}      @ Return
     
 shoot_up:
@@ -281,11 +324,26 @@ shoot_up:
     mov r4,r5
     push {lr}  
     ldr r11,=#0
+
     
-
+   
     pop {pc}
+move_left:
+    @ and Button A (Left) is pressed:
+        lsl r5, r5, #1    @ Move player left
+        cmp r5, #(1 << 5) @ Check if we went off the 5-bit screen
+        it hs             @ If Higher or Same
+        movhs r5, #(1 << 4) @ Snap back to leftmost column (bit 4)
+          bx lr
+move_right:
+    @ and Button B (Right) is pressed:
+        lsr r5, r5, #1    @ Move player right
+        cmp r5, #0        @ Check if we shifted out to zero
+        it eq
+        moveq r5, #1      @ Snap back to rightmost column (bit 0)
+      bx lr
 
-
+compare_score:
 
 @ Ram starting 0x20000000
 
